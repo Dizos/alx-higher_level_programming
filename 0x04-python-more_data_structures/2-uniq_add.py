257cca96cbbfca177808dc0cd4d7a9e278dd91ad
Tando Mjekula <tandomjekula@gmail.com>
2024-09-18 22:01:48 +0200

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    # Create a new list to store the results
-    new_list = []
-    
-    # Iterate through each element in the original list
-    for item in my_list:
-        # If the item matches the search element, append the replace element
-        # Otherwise, append the original item
-        if item == search:
-            new_list.append(replace)
-        else:
-            new_list.append(item)
-    
-    # Return the new list
-    return new_list
+def uniq_add(my_list=[]):
+    unique_nums = []
+    total = 0
+    for num in my_list:
+        if num not in unique_nums:
+            unique_nums.append(num)
+            total += num
+    return total
