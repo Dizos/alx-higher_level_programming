82c8d33c29e90bb90e3059bc6061523420347bc8
Tando Mjekula <tandomjekula@gmail.com>
2024-09-20 19:22:07 +0200

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ if not isinstance(roman_string, str) or roman_string is None:
 
     roman_values =
                     {
-                    I:1 ; V:5 ; X:10 ; L:50 ; C:100; D:500; M:1000
+                    I:1; V:5; X:10; L:50; C:100; D:500; M:1000
                     }
 
     Result = 0
