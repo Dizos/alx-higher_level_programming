d2b9d71f0c48553aa892c72f8d53366af3d3fe51
Tando Mjekula <tandomjekula@gmail.com>
2024-10-01 18:32:24 +0200

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square.
+ 
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+
+        Raises:
+            TypeError: If the size is not an integer.
+            ValueError: If the size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
