074290338598db53537f774004234b3c603a5580
Tando Mjekula <tandomjekula@gmail.com>
2024-10-01 18:25:23 +0200

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """starts a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
