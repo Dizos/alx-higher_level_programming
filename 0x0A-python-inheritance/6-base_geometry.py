82a5ced9fe889d1dfc92a8348273cd3db1e51ea1
Tando Mjekula <tandomjekula@gmail.com>
2024-10-14 18:09:49 +0200

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for improved BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Method to calculate area"""
+        raise Exception("area() is not implemented")
