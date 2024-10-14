bc3a1462099eab941178568c8e6ca4184cce853e
Tando Mjekula <tandomjekula@gmail.com>
2024-10-14 18:04:22 +0200

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class representing the base of geometry objects.
+    """
+    pass
