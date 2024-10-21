fa232f8d13e46145cbd329f0212c57f8b977842d
Tando Mjekula <tandomjekula@gmail.com>
2024-10-21 23:13:50 +0200

    ndanele

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,44 @@
 #!/usr/bin/python3
-"""This module defines the Base class."""
+"""This module defines the Base class
+   This module contains the Base class for the project."""
 
+import json
+
+
+class Base:
+    """
+    The base class for all other classes in this project.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize a new Base instance.
+
+        Args:
+            id (int, optional): The identity of the new Base instance.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Return the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+
+        Returns:
+            str: The JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
 class Base:
     """
